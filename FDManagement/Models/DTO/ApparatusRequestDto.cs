19f50879c88bf5036e5c9afe257c9bb933f40759
Jason Murphree <jtmurphree@gmail.com>
2024-04-04 13:54:43 -0500

    implemented dtos for apparatus

--- FDManagement/Models/DTO/ApparatusRequestDto.cs
+++ FDManagement/Models/DTO/ApparatusRequestDto.cs
@@ -2,5 +2,16 @@
 {
     public class ApparatusRequestDto
     {
+        public string UnitNum { get; set; }
+        public string Make {  get; set; }
+        public string Model { get; set; }
+        public int? Year { get; set; }
+        public int? Mileage { get; set; }
+        public DateTime? MileageDate { get; set; }
+        public int? ApparatusTypeID { get; set; }
+        public int? FuelTypeID { get; set; }
+        public int? DriveTypeID { get; set; }
+        public DateTime DateAdded {  get; set; }
+        public DateTime? DateUpdated { get; set; }
     }
 }
