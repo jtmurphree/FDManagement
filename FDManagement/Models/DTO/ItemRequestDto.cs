042b0b685fd4c5b07290fcd4be2ae94e18e563b1
Jason Murphree <jtmurphree@gmail.com>
2024-04-03 23:04:38 -0500

    Working on full implementation fo DTOs to uncouple from data

---
+++ FDManagement/Models/DTO/ItemRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FDManagement.Models.DTO
+{
+    public class ItemRequestDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string SerialNumber { get; set; }
+        public decimal? Value { get; set; }
+        public int? CategoryID { get; set; }
+    }
+}
--- FDManagement/Models/DTO/UserRequestDto.cs
+++ FDManagement/Models/DTO/UserRequestDto.cs
@@ -1,17 +1,22 @@
-﻿namespace FDManagement.Models.DTO
+﻿using System.Text;
+
+namespace FDManagement.Models.DTO
 {
     public class UserRequestDto
     {
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string UserName { get; set; }
         public string DisplayName { get; set; }
         public string EmployeeId { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
-        public int AccessFailedCount { get; set; }
+        public int? UserRoleId { get; set; }
+        public int? AccessFailedCount { get; set; }
         public DateTime DateAdded { get; set; }
         public DateTime? DateUpdated { get; set; }
+        public bool TempPw {  get; set; }
     }
 }
